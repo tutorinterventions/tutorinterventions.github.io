4a5fd74f60b78d50533e8932573039da3ecdf5d2
SteveMcArthur <contact@stevemcarthur.co.uk>
2018-07-31 16:57:57 +0100

    image fix and search fix

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 $(function() {
 
-     
+    
+    var focusOut = false; 
     $('.navbar-top .btn-search').click(function(ev){
         ev.preventDefault(); 
         $('.search-form .search-container').addClass('search-active');
+        if(!focusOut){
+            focusOut = true;
+            $('.gsc-input').focusout(function(ev){
+                $('.search-form .search-container').removeClass('search-active');
+            }); 
+        }
      });
 
      
-/*      $('.navbar-top .btn-search').focusout(function(ev){
+/*     $('.navbar-top .btn-search').focusout(function(ev){
         $('.search-form .search-container').removeClass('search-active');
-     }); */
+     });  */
      
 
 });
