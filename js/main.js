e06d1e7f5d112b183233950e13aacc77f1088499
SteveMcArthur <contact@stevemcarthur.co.uk>
2018-07-31 16:32:02 +0100

    added search

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@ $(function() {
      
     $('.navbar-top .btn-search').click(function(ev){
         ev.preventDefault(); 
-        $('.search-form input').toggleClass('search-active');
+        $('.search-form .search-container').addClass('search-active');
      });
+
+     
+/*      $('.navbar-top .btn-search').focusout(function(ev){
+        $('.search-form .search-container').removeClass('search-active');
+     }); */
+     
 
 });
