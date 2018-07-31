178fd2d80e19a4f382174cc491acc1ca07be566d
SteveMcArthur <contact@stevemcarthur.co.uk>
2018-07-31 18:27:51 +0100

    Privacy policy and cookie consent

---
+++ js/test-forms.js
@@ -0,0 +1,14 @@
+$("input#subjects").val("Dark Matter Baking");
+$("input#location").val("Los Angeles");
+$("input#first-name").val("Dara");
+$("input#last-name").val("O'Briain");
+$("input#email").val("json@live.co.uk");
+$("input#phone").val("07123456789");
+$("textarea#message").val("Toby or not Toby that is the quisling");
+
+
+
+$("input#name").val("Tom Baker");
+$("input#email").val("json@live.co.uk");
+$("input#phone").val("07123456789");
+$("textarea#message").val("Whats the point of being an adult if you can't be childish sometimes?");
