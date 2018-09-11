7d70b88afdb02d5810485fba3ca139f9bb898160
SteveMc <contact@stevemcarthur.co.uk>
2018-09-11 08:34:51 +0100

    fixed button colours

--- js/schools.js
+++ js/schools.js
@@ -1,26 +1,11 @@
-$(function(){
+$(function () {
 
-     // Initialize Firebase
-     var config = {
-        apiKey: "AIzaSyAD15SQTbfsE5sYrARvws3MzCaskXmzwjQ",
-        authDomain: "tutor-interventi-1523169861738.firebaseapp.com",
-        databaseURL: "https://tutor-interventi-1523169861738.firebaseio.com",
-        projectId: "tutor-interventi-1523169861738",
-        storageBucket: "tutor-interventi-1523169861738.appspot.com",
-        messagingSenderId: "122898722954"
-    };
-    firebase.initializeApp(config);
-    var ref = firebase.database().ref();
-
-    ref.on("value", function (snapshot) {
-        var data = snapshot.val();
-
+    function displayData(data) {
         $('#schools-table').bootstrapTable({
             data: data,
             pagination: true,
             search: true,
             sortable: true,
-            toolbar: "#toolbar",
             columns: [{
                 field: 'URN',
                 title: 'URN',
@@ -39,7 +24,28 @@ $(function(){
                 sortable: true
             },]
         });
-    });
+    }
+
+    function initFirebase() {
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyAD15SQTbfsE5sYrARvws3MzCaskXmzwjQ",
+            authDomain: "tutor-interventi-1523169861738.firebaseapp.com",
+            databaseURL: "https://tutor-interventi-1523169861738.firebaseio.com",
+            projectId: "tutor-interventi-1523169861738",
+            storageBucket: "tutor-interventi-1523169861738.appspot.com",
+            messagingSenderId: "122898722954"
+        };
+        firebase.initializeApp(config);
+        var ref = firebase.database().ref();
+
+        ref.on("value", function (snapshot) {
+            var data = snapshot.val();
+            displayData(data);
+        });
+    }
+
+    initFirebase();
 
 
 });
