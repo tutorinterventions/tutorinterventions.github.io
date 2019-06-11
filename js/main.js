2b0552c5b12d96081f0aa4a2df2db2d494447d73
SteveMc <contact@stevemcarthur.co.uk>
2019-06-11 15:25:49 +0100

    Added testimonials slider

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
-$(function() {
+$(function () {
 
-    
-    var focusOut = false; 
-    $('.navbar-top .btn-search').click(function(ev){
-        ev.preventDefault(); 
+
+    var focusOut = false;
+    $('.navbar-top .btn-search').click(function (ev) {
+        ev.preventDefault();
         $('.search-form .search-container').addClass('search-active');
-        if(!focusOut){
+        if (!focusOut) {
             focusOut = true;
-            $('.gsc-input').focusout(function(ev){
+            $('.gsc-input').focusout(function (ev) {
                 $('.search-form .search-container').removeClass('search-active');
-            }); 
+            });
         }
-     });
+    });
+
+    $('.testimonials-wrapper').on('init', function () {
+        $(this).removeClass("fh");
+    });
+
+    $('.testimonials-wrapper').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 30000,
+        dots: true
+    });
+
 
 });
